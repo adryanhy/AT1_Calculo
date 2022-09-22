08eb7f0637d3dec6afb1fc8f94f14c8d686091a5
Adryan <100885847+adryanhy@users.noreply.github.com>
2022-09-22 01:12:45 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,40 +1,120 @@
 #include <math.h> //Biblioteca para usar sin, cos, tg
+#include <stdio.h>
 #include <stdlib.h>
 #define PI 3.14159265 //Constante de Pi
 
+                        /* ATIVIDADE 1 - CALCULO - ADS */
+                /* Adryan, Bruno Ricardo, Luana, Lucas e Higor */
 int main() {
     int op;
-    double angulo, result, parametro, x;
+    float resultado, num, resultadoInvalido;
+    double angulo,  x, k;
     printf("\tMENU\n");
     printf("Digite a opcao desejada\n");
     printf("1. Sen(x)\n");
     printf("2. Cos(x)\n");
     printf("3. Tg(x)\n");
+    printf("4. f(x) = x^k\n");
+    printf("5. f(x) = 1/x\n");
+    printf("6. SAIR\n");
     scanf("%d", &op);
+
     switch (op) {
         case 1:
+            system("cls");
             printf("Calculo de Seno\n");
-            printf("Insira o valor do angulo(x): ");
-            scanf("%lf", &x);
-            parametro = x;
-            result = sin(parametro *PI / 180); //Resultado recebe a função do sin com a conversão para radiano
-            printf("O Seno de (%.2lf):  %.4lf rad", parametro, result);
+            printf("Insira o valor do seno(x): ");
+            scanf("%f", &num);
+            resultado = sin(num);
+            printf("Resultado do seno (x): %.4f radianos", resultado);
+            printf("\n6. Voltar ao menu principal:\n\n");
+            scanf("%d",&op);
+            system("cls");
+            if(op == 6)
+            {
+                system("cls");
+                return main ();
+            }
             break;
+
 
         case 2:
+            system("cls");
             printf("Calculo de Cosseno\n");
-            printf("Insira o valor do angulo(x): ");
-            scanf("%lf", &x);
-            parametro = x;
-            result = cos(parametro * PI / 180); //Resultado recebe a função do cos com a conversão para radiano
-            printf("O Cosseno de (%.2lf):  %.4lf rad", parametro, result);
+            printf("Insira o valor de (x): ");
+            scanf("%f", &num);
+            resultado = cos(num);
+            printf("Resultado do cosseno de (%.2f): %.4f radianos", num, resultado);
+            printf("\n6. Voltar ao menu principal:\n\n");
+            scanf("%d",&op);
+            if(op == 6)
+            {
+                system("cls");
+                return main ();
+            }
             break;
 
-        /*case 3:
-            printf("Calculo de Tangente\n");
-            printf("Insira o valor do angulo(x): ");
+            case 3:
+                system("cls");
+                printf("Calculo de Tangente\n");
+                printf("Insira o valor do angulo(x): ");
+                scanf("%lf", &x);
+                resultadoInvalido = fmod(x, 90);    //fmod pega o resto da divisão por float
+                resultado = tan(x);
+                if (resultado == resultadoInvalido){
+                    printf("Resultado Invalido");
+                } else{
+                printf("%.4lf radianos", resultado);
+                }
+                printf("\n6. Voltar ao menu principal:\n\n");
+                scanf("%d",&op);
+                if(op == 6)
+                {
+                    system("cls");
+                    return main ();
+                }
+                break;
+
+        case 4:
+            system("cls");
+            printf("Calculo de f(x)= x^k\n");
+            printf("Insira o valor de x: ");
             scanf("%lf", &x);
-            break;*/
+            if (x == 0) {
+                printf("Valor de X invalido");
+            }else {
+                printf("Insira o valor de k: ");
+                scanf("%lf", &k);
+                resultado = pow(x, k);
+                printf("O resultado de f(%.2lf^%.2lf) = %.4f", x, k, resultado);
+            }
+            printf("\n6. Voltar ao menu principal:\n\n");
+            scanf("%d",&op);
+            if(op == 6)
+            {
+                system("cls");
+                return main ();
+            }
+            break;
+
+        case 5:
+            system("cls");
+            printf("Calculo de f(x)\n");
+            printf("Insira o valor de x: ");
+            scanf("%lf", &x);
+            if (x == 0){
+                printf("Valor de X invalido, digite um numero diferente de 0");
+            }
+            resultado= 1/x;
+            printf("O resultado de f(%.2lf) = %.4lf", x, resultado);
+            printf("\n6. Voltar ao menu principal:\n\n");
+            scanf("%d",&op);
+            if(op == 6)
+            {
+                system("cls");
+                return main ();
+            }
+            break;
 
         default:
             printf("Opcao invalida");
